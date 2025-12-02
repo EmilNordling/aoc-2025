455e470a8ed4cb069814bdee9ab2e944b76d5614
Emil Nordling <i.emil.nordling@gmail.com>
2025-12-02 11:42:38 +0100

    updates test input

--- go/year2025/day01/day01_test.go
+++ go/year2025/day01/day01_test.go
@@ -2,8 +2,19 @@ package day01
 
 import "testing"
 
+const TEST_INPUT = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
 func TestPart1(t *testing.T) {
-	input := Parse("R10\nL5\nR15")
+	input := Parse(TEST_INPUT)
 	result := Part1(&input)
 	if result < 0 {
 		t.Errorf("Part1Day01() returned negative value: %d", result)
@@ -11,7 +22,7 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	input := Parse("R10\nL5\nR15")
+	input := Parse(TEST_INPUT)
 	result := Part2(&input)
 	if result < 0 {
 		t.Errorf("Part2Day01() returned negative value: %d", result)
