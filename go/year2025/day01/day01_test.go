d038f62a5555a3ef4756166e8b7b2374bb998a3e
Emil Nordling <i.emil.nordling@gmail.com>
2025-12-02 11:36:52 +0100

    updates test naming

--- go/year2025/day01/day01_test.go
+++ go/year2025/day01/day01_test.go
@@ -2,7 +2,7 @@ package day01
 
 import "testing"
 
-func TestPart1Day01(t *testing.T) {
+func TestPart1(t *testing.T) {
 	input := Parse("R10\nL5\nR15")
 	result := Part1(&input)
 	if result < 0 {
@@ -10,7 +10,7 @@ func TestPart1Day01(t *testing.T) {
 	}
 }
 
-func TestPart2Day01(t *testing.T) {
+func TestPart2(t *testing.T) {
 	input := Parse("R10\nL5\nR15")
 	result := Part2(&input)
 	if result < 0 {
